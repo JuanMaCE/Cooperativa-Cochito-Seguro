0fb24cf7dc80396d2c43f67eae4d9f5482a9de51
Vyncen Lara <vyncen.lara@gmail.com>
2024-03-20 17:49:12 -0600

    funcion solicitar para que jose la agregue a la interfaz

--- prestamo/prestamo.py
+++ prestamo/prestamo.py
@@ -1,9 +1,12 @@
+import random
+
+
 class Prestamo:
-    def __init__(self, id_prestamo: int, id_credito: int, status: str, monto_solicitado: int,
-                 cuotas: int, monto_aprobado: int, ingresos: int, garantia: str,
-                 archivos, plan_pagos, historial_pagos):
+    def __init__(self, id_prestamo: int, id_asociado: int, status: str, monto_solicitado: float,
+                 cuotas: int, monto_aprobado: float, ingresos: float, garantia: str,
+                 archivos, plan_pagos: str, historial_pagos):
         self.id_prestamo = id_prestamo
-        self.id_credito = id_credito
+        self.id_asociado = id_asociado
         self.status = status
         self.monto_solicitado = monto_solicitado
         self.coutas = cuotas
@@ -14,18 +17,54 @@ class Prestamo:
         self.plan = plan_pagos
         self.historial_pagos = historial_pagos
 
-    def solicitar(self):
-        pass
+    def transversal(self) -> str:
+        result = f"Id prestamo: {self.id_prestamo}, "
+        result += f"asociado: {self.id_asociado}, "
+        result += f"status: {self.status}, "
+        result += f"monto sol: {self.monto_solicitado}, "
+        result += f"cuotas: {self.coutas}, "
+        result += f"monto aprobado: {self.monto_aprobado}, "
+        result += f"ingrresos: {self.ingresos}, "
+        result += f"garantia: {self.garantia}, "
+        result += f"archivos: {self.archivos}, "
+        result += f"plan: {self.plan}, "
+        result += f"historial: {self.historial_pagos}, "
+        return result
 
-    def generar_plan(self):
-        pass
 
-    def aprobar(self):
-        pass
+def solicitar():
+    monto_solicitado = float(input("Monto solicitado, LAYUOT: "))
+    num_prestamo = random.randint(100, 999)
+    ingresos = float(input("Ingresos mensuales, LAYOUT: "))
+    cuotas = int(input("Coutas solicitadas, LAYOUT: "))
+    capacidad_de_pago = ingresos * 0.1 * cuotas
+    monto_solicitado_interes = ((monto_solicitado * ((cuotas//3) * 0.05)) + monto_solicitado)
+    if capacidad_de_pago >= monto_solicitado_interes:
+        monto_aprobado = monto_solicitado_interes
+    else:
+        monto_aprobado = capacidad_de_pago
+    garantia = input("GARANTIA, LAYOUT: ")
+    archivo = input("ARCHIVOS, LAYOUT: ")
+    plan_pagos = "Numero de Pagos: ", cuotas, "Pago minimo en cuota: ", str(float(monto_aprobado / cuotas))
+    historial_pagos = 0
+    x = Prestamo(num_prestamo, 10001, "Generado",  monto_solicitado, cuotas, monto_aprobado, ingresos,
+             garantia, archivo, plan_pagos, historial_pagos)
+    print(x.transversal())
 
-    def visualizar(self):
-        pass
 
-    def realizar_pago(self):
-        pass
+solicitar()
+solicitar()
+def generar_plan():
+    pass
 
+
+def aprobar():
+    pass
+
+
+def visualizar():
+    pass
+
+
+def realizar_pago():
+    pass
