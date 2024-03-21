8a21ed7df2e6afc8e10ef0476de294f02aa3073e
Juan Marcos Cortés Enriquez <124099699+JuanMaCE@users.noreply.github.com>
2024-03-21 00:45:03 -0600

    se implementaron asociados predeterminandos, solo para demostracion

--- main.py
+++ main.py
@@ -7,8 +7,8 @@ from gestion_prestamo import menu_prestamos
 app = customtkinter.CTk()
 
 
-def open_gestion():
-    menu_gestion_asociados.main()
+def open_gestion(list_asociaods):
+    menu_gestion_asociados.main(list_asociaods)
 
 
 def open_users(user_pred):
@@ -19,7 +19,7 @@ def open_prestamo():
     menu_prestamos.main()
 
 
-def main_window(lista: List):
+def main_window(list_usuarios, list_asociaods):
     customtkinter.set_appearance_mode("dark")
     customtkinter.set_default_color_theme("blue")
 
@@ -28,27 +28,30 @@ def main_window(lista: List):
     app.wm_attributes("-topmost", False)
     frame = customtkinter.CTkFrame(master=app)
     frame.pack(pady=10, padx=10, fill='both', expand=True)
-    buttons(frame, lista)
+    buttons(frame, list_usuarios, list_asociaods)
 
     # botones
     app.mainloop()
 
 
-def buttons(frame, lista):
+def buttons(frame, list_usuarios, list_asociaods):
     button_gestion = customtkinter.CTkButton(master=frame, text='Gestión de Asociados', height=100, width=150,
-                                         font=("Arial", 20), fg_color="#3E4446", command=lambda: open_gestion())
+                                             font=("Arial", 20), fg_color="#3E4446",
+                                             command=lambda: open_gestion(list_asociaods))
 
     button_gestion.pack(pady=10, padx=10)
     button_gestion.place(x=50, y=10)
 
     bt_prestamos_bancarios = customtkinter.CTkButton(master=frame, text='Prestamos bancarios', height=100, width=160,
-                                                     font=("Arial", 20), fg_color="#3E4446", command=open_prestamo)
+                                                     font=("Arial", 20), fg_color="#3E4446",
+                                                     command=lambda: open_prestamo)
 
     bt_prestamos_bancarios.pack(pady=10, padx=10)
     bt_prestamos_bancarios.place(x=50, y=150)
 
     bt_usuarios = customtkinter.CTkButton(master=frame, text='Usuarios', height=100, width=210,
-                                          font=("Arial", 20), fg_color="#3E4446", command=lambda: open_users(lista))
+                                          font=("Arial", 20), fg_color="#3E4446",
+                                          command=lambda: open_users(list_usuarios))
 
     bt_usuarios.pack(pady=10, padx=10)
     bt_usuarios.place(x=50, y=310)
